36548ac945a3f84cd055cab371fd375f9cf1e910
Ainoa <aimmolina2@gmail.com>
2026-03-09 20:48:44 +0100

    basic login layout

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -1,13 +1,20 @@
-import { appRoutes } from "@/core/router";
 import React from "react";
-import { Link } from "react-router-dom";
 
 export const LoginPage: React.FC = () => {
   return (
     <div>
-      Login
-      <br />
-      <Link to={appRoutes.accountList}>Mis cuentas</Link>
+      <h1>Acceso</h1>
+      <form>
+        <div>
+          <label htmlFor="username">Usuario</label>
+          <input type="text" id="username"></input>
+        </div>
+        <div>
+          <label htmlFor="password">Contraseña</label>
+          <input type="passwprd" id="password"></input>
+        </div>
+        <button type="submit">Acceder</button>
+      </form>
     </div>
   );
 };
