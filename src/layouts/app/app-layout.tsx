176f0d859a815fe812e6e3d002eb84b9fff1fd54
Ainoa <aimmolina2@gmail.com>
2026-03-12 17:03:09 +0100

    header layout style ready

---
+++ src/layouts/app/app-layout.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { HeaderComponent } from "./components";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const AppLayout: React.FC<Props> = (props) => {
+  const { children } = props;
+
+  return (
+    <>
+      <HeaderComponent />
+      <h1>Aquí iría la cabecera</h1>
+      {children}
+      <h1>Aquí iría el footer</h1>
+    </>
+  );
+};
